7c60eb278f0dcaf8b7f3ab6509a255564d3cfd78
randomDam <damien.bais@gmail.com>
2023-03-29 15:40:43 +0200

    add modif

--- PersoFPS.gd
+++ PersoFPS.gd
@@ -9,25 +9,31 @@ var look_sensitivity = 0.002
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var velocity_y = 0
 
-
+#----------------------------------------------------------------------------
+# PROCESS PHYSIC
+#----------------------------------------------------------------------------
 func _physics_process(delta):
+	# deplacement du personnage
 	var horizontal_velocity = Input.get_vector("move_left","move_right","move_forward","move_backward") * speed
 	velocity = horizontal_velocity.x * global_transform.basis.x + horizontal_velocity.y * global_transform.basis.z
 	if is_on_floor():
-		velocity_y = jump_velocity if Input.is_action_just_pressed("jump") else 0
+		if Input.is_action_just_pressed("jump"):
+			velocity_y = jump_velocity 
 	else: 
 		velocity_y -= gravity * delta
-		velocity.y = velocity_y
+	
+	velocity.y = velocity_y
 	move_and_slide()
 	
-	
-	var view = Input.get_vector("view_left","view_right","view_up","view_down");
-	
+	#---------------------------------------------------------------
+	# Gestion de la vue
+	var view = Input.get_vector("view_left","view_right","view_up","view_down")
 	rotate_y(-view.x * 0.01)
 	camera.rotate_x(-view.y * 0.01)
 	camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
 	
 	#---------------------------------------------------------------
+	# Gestion de la sortie du curseur exclusif
 	if Input.is_action_just_pressed("ui_cancel"): 
 			immersif=!immersif
 	if immersif:
@@ -36,7 +42,8 @@ func _physics_process(delta):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
 	
-	
+#---------------------------------------------------------------
+# recuparation de la souris
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * look_sensitivity)
