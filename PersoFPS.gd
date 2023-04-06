3f1d6c26681cf1cd2113a0aae833b276d26058cc
randomDam <damien.bais@gmail.com>
2023-04-06 09:43:03 +0200

    bug fps script

--- PersoFPS.gd
+++ PersoFPS.gd
@@ -19,6 +19,8 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			velocity_y = jump_velocity 
+		else:
+			velocity_y = 0
 	else: 
 		velocity_y -= gravity * delta
 	
